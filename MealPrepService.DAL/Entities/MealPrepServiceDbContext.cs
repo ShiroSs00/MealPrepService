6507815482eca1878ef50302d3f20d6846ce691e
Hồ Trần Quân <quanhojkj5@gmail.com>
2026-01-21 14:21:45 +0700

    Initial commit: 3-layer ASP.NET Core MealPrepService

---
+++ MealPrepService.DAL/Entities/MealPrepServiceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPrepService.DAL.Entities
+{
+    public class MealPrepDbContext : DbContext
+    {
+        public MealPrepDbContext(DbContextOptions<MealPrepDbContext> options) : base(options) { }
+    }
+}
